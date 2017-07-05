46ce5a9316e886e596e64bbfaa5edd6bbe737b04
Arthur Asatryan <biacoder@gmail.com>
2017-07-05 22:25:09 +0400

    #21 Some boy scouting

--- examples/main.rs
+++ examples/main.rs
@@ -7,10 +7,13 @@ use docker4rs::docker::DockerCredentials;
 use std::sync::Arc;
 use hyper::Client;
 
+const DOCKER_API_ADDRESS: &'static str = "http://localhost";
+const DOCKER_API_PORT: &'static i32 = 2375;
+
 fn main() {
     let client = Arc::new(Client::new());
     let containers = Container::get_running_containers(
-        &DockerCredentials::new("http://localhost".to_string(), 2375),
+        &DockerCredentials::new(DOCKER_API_ADDRESS.to_string(), DOCKER_API_PORT),
         &client.clone()
     );
     println!("The running containers {:?}", containers);
