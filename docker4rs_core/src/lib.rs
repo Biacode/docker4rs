4ae0cea670d3281239347abe16dacc422996d424
Arthur Asatryan <biacoder@gmail.com>
2017-07-09 00:57:26 +0400

    #17 #24 Boy scouting in workspaces

---
+++ docker4rs_core/src/lib.rs
@@ -0,0 +1,10 @@
+pub struct Address {
+    first_name: String,
+    last_name: String
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {}
+}
---
+++ docker4rs_tcp/src/lib.rs
@@ -0,0 +1,10 @@
+pub struct User {
+    first_name: String,
+    last_name: String
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {}
+}
