310515cb1e1cd4b1a0a730ff5825eeee2ea07c77
Arthur Asatryan <biacoder@gmail.com>
2017-07-09 21:19:13 +0400

    #22 Added event loop example

--- examples/event_loop.rs
+++ examples/event_loop.rs
@@ -1,8 +1,9 @@
 extern crate futures;
 extern crate tokio_core;
+extern crate tokio_io;
 
-use futures::Stream;
-use tokio_core::net::TcpListener;
+use futures::{Future, Stream};
+use tokio_core::net::{TcpListener, TcpStream};
 use tokio_core::reactor::Core;
 
 fn main() {
@@ -11,12 +12,17 @@ fn main() {
         &"127.0.0.1:8080".parse().unwrap(),
         &core.handle()
     ).unwrap();
-
-    let server = listener.incoming().for_each(|(client, client_addr)| {
-        // process `client` by spawning a new task ...
-
+    let handle = core.handle();
+    let server = listener.incoming().for_each(|(client, _client_addr)| {
+        handle.spawn(process(client));
         Ok(()) // keep accepting connections
     });
-
     core.run(server).unwrap();
+}
+
+// Here we'll express the handling of `client` and return it as a future
+// to be spawned onto the event loop.
+fn process(client: TcpStream) -> Box<Future<Item=(), Error=()>> {
+    println!("Got client {:?}", client);
+    tokio_io::io::write_all(client, b"Hello, World\n").then(|_| Ok(())).boxed()
 }
